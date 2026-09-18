37e8d6d535ea6dbded463e24a6ebff8c25a5d567
Sabi Mantock <sabimantock@gmail.com>
2026-09-18 12:56:13 +0100

    Use Cloudinary video for Dkleener preview

--- src/components/portfolio/projects/dkleener-preview.tsx
+++ src/components/portfolio/projects/dkleener-preview.tsx
@@ -27,7 +27,10 @@ export function DkleenerPreview() {
           borderRadius: 14,
         }}
       >
-        <source src="/videos/dkleener-preview.mp4" type="video/mp4" />
+        <source
+          src="https://res.cloudinary.com/ez9kcige/video/upload/v1789732234/Screen_Recording_2026-09-18_at_11.04.58_wkfasv.mp4"
+          type="video/mp4"
+        />
         Your browser does not support this video.
       </video>
     </div>
